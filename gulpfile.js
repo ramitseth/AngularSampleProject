87e073ffbc5fce801ff4c8e2afe86001d40e5871
Ramit Seth <ramit.pgdav@gmail.com>
2018-01-04 01:18:59 +0530

    Adding gitignore file and removing extra bower components

--- gulpfile.js
+++ gulpfile.js
@@ -31,8 +31,6 @@ var path = {
     'src/assets/lib/**/*.js'
   ],
   JS_BODY: [
-  	'src/assets/js/*.js',
-    'src/assets/js/**/*.js',
     'src/app/*.js',
     'src/app/**/*.js',
     'src/app/**/**/*.js',
@@ -53,16 +51,12 @@ var path = {
   ],
   BOWER_COMPONENTS: [
   	'bower_components/angular/angular.js', 
-    'bower_components/angular-animate/angular-animate.js', 
     'bower_components/angular-aria/angular-aria.js', 
-    'bower_components/angular-cookies/angular-cookies.js', 
     'bower_components/angular-loader/angular-loader.js', 
-    'bower_components/angular-mocks/angular-mocks.js', 
     'bower_components/angular-resource/angular-resource.js', 
-    'bower_components/angular-sanitize/angular-sanitize.js', 
+    'bower_components/angular-sanitize/angular-sanitize.js',
     'bower_components/angular-touch/angular-touch.js',  
-    'bower_components/angular-ui-router/release/angular-ui-router.js',
-    'bower_components/jquery/dist/jquery.js'
+    'bower_components/angular-ui-router/release/angular-ui-router.js'
   ],
   SRC: './src',
   DEV: './dev',
